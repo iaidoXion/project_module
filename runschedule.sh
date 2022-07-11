93bdd3d32d397550792a821809be60c6fb5d1bc1
skekehdwn <krtdj@naver.com>
2022-07-11 12:59:53 +0900

    project_module/runschedule.sh 파일 추가  - crontab을 이용한 스케줄링 쉘파일 추가  - 매일 0시 1분에 자동실행

---
+++ runschedule.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source /venvs/xion/bin/activate
+
+cd /venvs/xion/bin/projects/project_module
+
+python main.py
+
+
+#pwd
+#sleep 1
+#source /venvs/xion/bin/activate
+#sleep 1
+#date>> /root/test.txt
+#pwd>> /root/test.txt
+
+
