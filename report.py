955370affcd7095a8933802bb7551da62f4d4ec5
iaido <iaido@xionits.com>
2022-11-11 10:04:35 +0900

    report module 개발

---
+++ report.py
@@ -0,0 +1,35 @@
+from Core.Report import plug_in as RPI
+from datetime import datetime
+import urllib3
+import json
+import logging
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+with open("setting.json", encoding="UTF-8") as f:
+    SETTING = json.loads(f.read())
+waitingUse = SETTING['PROJECT']['WAITING']['USE']
+waitingDate = SETTING['PROJECT']['WAITING']['DATE']
+logFileDirectory = SETTING['LOG']['directory']
+logFileName = SETTING['LOG']['fileName']
+logFileFormat = SETTING['LOG']['fileFormat']
+TU = SETTING['CORE']['Tanium']['USE']
+ZU = SETTING['CORE']['Zabbix']['USE']
+RU = SETTING['Report']['USE']
+RDU = SETTING['Report']['DAILY']['USE']
+RWU = SETTING['Report']['WEEKLY']['USE']
+RMU = SETTING['Report']['MONTHLY']['USE']
+
+def main() :
+    if RU == 'true' :
+        if RDU == 'true' :
+            RPI()
+
+if __name__ == "__main__":
+    today = datetime.today().strftime("%Y%m%d")
+    logFile = logFileDirectory + logFileName + today + logFileFormat
+    logFormat = '%(levelname)s, %(asctime)s, %(message)s'
+    logDateFormat = '%Y%m%d%H%M%S'
+    logging.basicConfig(filename=logFile, format=logFormat, datefmt=logDateFormat, level=logging.DEBUG)
+    logging.info('Module Started')
+    main()
+    logging.info('Module Finished')
